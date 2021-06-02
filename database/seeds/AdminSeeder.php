04452bcfd513359921dc3e98a520cd3de1643129
Anisapandu23 <anisapandundis1@gmail.com>
2021-06-03 00:51:40 +0700

    create database but error

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Admin;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'username' => 'admin',
+            'password' => bcrypt(1234567890)
+        ]);
+    }
+}
